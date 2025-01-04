cd46ba58b8fb299fff0489bffa44ea9be29d675b
sagarikaghom14 <sagarika.ghom@mitaoe.ac.in>
2025-01-04 19:38:28 +0530

    completed routing

---
+++ Frontend/src/pages/CaptainHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CaptainHome = () => {
+  return (
+    <div>
+      Cap Home
+    </div>
+  )
+}
+
+export default CaptainHome
--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -1,30 +1,59 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-
+import { Link ,useNavigate} from 'react-router-dom'
+import { CaptainDataContext } from '../context/CaptainContext'
+import axios from 'axios'
 
 const CaptainSignup = () => {
 
-  const [email,setEmail] = useState('')
-  const [password,setPassword]=useState('')
-  const [firstName,setFirstName] = useState('')
-  const [lastName,setLastName]=useState('')
-  const [userData,setUserData]= useState({})
+  const navigate = useNavigate()
 
-  const submitHandler=(e)=>{
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [userData, setUserData] = useState({})
+
+  const [vehicleColor, setVehicleColor] = useState('')
+  const [vehiclePlate, setVehiclePlate] = useState('')
+  const [vehicleCapacity, setVehicleCapacity] = useState('')
+  const [vehicleType, setVehicleType] = useState('')
+
+  const { captain, setCaptain } = React.useContext(CaptainDataContext)
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setUserData({
-      fullName:{
-        firstName:firstName,
-        lastName:lastName
+     const captainData ={
+      fullname: {
+        firstname: firstName,
+        lastname: lastName
       },
-      email:email,
-      password:password
-    })
-    
+      email: email,
+      password: password,
+      vehicle: {
+        color: vehicleColor,
+        plate: vehiclePlate,
+        capacity: vehicleCapacity,
+        vehicleType: vehicleType
+      }
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/register`, captainData)
+
+    if (response.status === 201) {
+      const data = response.data
+      setCaptain(data.captain)
+      localStorage.setItem('token', data.token)
+      navigate('/captain-home')
+    }
+
     setEmail('')
-    setPassword('')
     setFirstName('')
     setLastName('')
+    setPassword('')
+    setVehicleColor('')
+    setVehiclePlate('')
+    setVehicleCapacity('')
+    setVehicleType('')
   }
   return (
     <div>
@@ -43,7 +72,7 @@ const CaptainSignup = () => {
                 type="text"
                 placeholder='First name'
                 value={firstName}
-                onChange={(e)=>{
+                onChange={(e) => {
                   setFirstName(e.target.value)
                 }}
               />
@@ -53,7 +82,7 @@ const CaptainSignup = () => {
                 type="text"
                 placeholder='Last name'
                 value={lastName}
-                onChange={(e)=>{
+                onChange={(e) => {
                   setLastName(e.target.value)
                 }}
               />
@@ -66,7 +95,7 @@ const CaptainSignup = () => {
               type="email"
               placeholder='email@example.com'
               value={email}
-              onChange={(e)=>{
+              onChange={(e) => {
                 setEmail(e.target.value)
               }}
             />
@@ -78,20 +107,69 @@ const CaptainSignup = () => {
               type="password"
               placeholder='password'
               value={password}
-              onChange={(e)=>{
+              onChange={(e) => {
                 setPassword(e.target.value)
               }}
             />
 
+            <h3 className='text-lg font-medium mb-2'>Vehicle Information</h3>
+            <div className='flex gap-4 mb-7'>
+              <input
+                required
+                className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                type="text"
+                placeholder='Vehicle Color'
+                value={vehicleColor}
+                onChange={(e) => {
+                  setVehicleColor(e.target.value)
+                }}
+              />
+              <input
+                required
+                className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                type="text"
+                placeholder='Vehicle Plate'
+                value={vehiclePlate}
+                onChange={(e) => {
+                  setVehiclePlate(e.target.value)
+                }}
+              />
+            </div>
+            <div className='flex gap-4 mb-7'>
+              <input
+                required
+                className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                type="number"
+                placeholder='Vehicle Capacity'
+                value={vehicleCapacity}
+                onChange={(e) => {
+                  setVehicleCapacity(e.target.value)
+                }}
+              />
+              <select
+                required
+                className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+                value={vehicleType}
+                onChange={(e) => {
+                  setVehicleType(e.target.value)
+                }}
+              >
+                <option value="" disabled>Select Vehicle Type</option>
+                <option value="car">Car</option>
+                <option value="auto">Auto</option>
+                <option value="moto">Moto</option>
+              </select>
+            </div>
+
             <button
               className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'
-            >Create account</button>
+            >Create Captain account</button>
 
           </form>
           <p className='text-center'>Already have a account? <Link to='/captain-login' className='text-blue-600'>Login here</Link></p>
         </div>
         <div>
-        <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
+          <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
             Policy</span> and <span className='underline'>Terms of Service apply</span>.</p>
         </div>
       </div>
